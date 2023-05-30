d42c865b0659deb996cb3c5137fbc34b34f825ea
MagCost <113210044+MagCost@users.noreply.github.com>
2023-05-30 22:31:05 +0200

    updated coordinates function

--- index.js
+++ index.js
@@ -38,16 +38,14 @@ function displaySearchedCity(event) {
   function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
     let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
 
     return days[day];
   }
 
   function displayForecast(response) {
     let forecast = response.data.daily;
-
     let forecastElement = document.querySelector("#forecast");
-
     let forecastHTML = `<div class="row">`;
     forecast.forEach(function (forecastDay, index) {
       if (index < 6) {
@@ -78,6 +76,7 @@ function displaySearchedCity(event) {
     let lon = coordinates.lon;
     let apiKey = "c819171fe0abdc14039af4ef5dda283b";
     let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiURL).then(displayForecast);
   }
 
   //function to update temperature, wind, humidity, icon and description
